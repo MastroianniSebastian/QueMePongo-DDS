46e0ed46688afa88db39aae526712682c7ac5a30
smastroianni <smastroianni@abs-ti.com>
2023-05-18 22:51:16 -0300

    Se arregla error de tipeo

--- src/main/java/ar/edu/utn/frba/dds/ServicioAccuWeather.java
+++ src/main/java/ar/edu/utn/frba/dds/ServicioAccuWeather.java
@@ -14,6 +14,6 @@ public class ServicioAccuWeather implements ServicioClimatico{
     List<Map<String, Object>> condicionesClimaticas = apiClima.getWeather(ciudad);
     //TODO Hacer mock para probar y evitar incurrir en costos innecesarios
 
-    return (Integer) condicionesClimaticas.get(0).get("Temperature");git sgit 
+    return (Integer) condicionesClimaticas.get(0).get("Temperature");
   }
 }
